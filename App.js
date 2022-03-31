6670939961e33c36c891db7f3a20f5d582d702f2
paragtaide <89142951+paragtaide@users.noreply.github.com>
2022-03-31 18:47:19 +0530

    Reviews Page

--- App.js
+++ App.js
@@ -1,95 +1,48 @@
-import React  from "react";
+import logo from './logo.svg';
+import './App.css';
 
-class TnC extends React.Component
-{
-    render(){
-        return(
-            <div>
-                 <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-lg-9">
-                        <div className="card shadow border-0 rounded">
-                            <div className="card-body"><br/>
-                                <h1 className="card-title" style={{textAlign:"center"}}>ABOUT US</h1>
-                                <p className="text-muted" style={{textAlign:"right"}}>Effective Date: 1st Aprils 2022</p>
+function App() {
+  return (
 
-                                <h3 className="card-title">"WE ARE AT GET YOUR DRIVER."</h3>
-                                <p className="text-muted">- We are a bunch of dynamic folks, who loves to travel all over the world. Our vision is to offer best services to all travel lovers and to make their trip amazing by providing them verified screened drivers.</p>
-                                <p className="text-muted">-   Get Your Driver was established in 2022 by Our DOH Team. We have successfully offered our best services to more than 1000 people and currently we have 100 plus drives associated with us and 200 active drives to fulfill our customer’s requirement on hourly basis, daily basis and monthly basis. We want to make your trip more thrilling than you have ever thought. If you love to travel then here we are!! Try our services to have a wonderful experience with us.</p>
-                              
-                                
-                                <h3 className="card-title">"TRANSPERANCY"</h3>
-                                 <p className="text-muted">- Most importantly, you can speak to your service provider before confirming your booking so that you can clear all your doubts.Booking process is simple : Enter Journey Details, Choose your Price, Speak to driver and Done.</p>
-                                 <p className="text-muted">- Transparent billing.</p>
-                                 <p className="text-muted">- Our core service is developing technology that connects private and commercial vehicle owners with drivers on demand.</p>
+    <div className="card-body"><br/>
+    <h1 className="card-title" style={{textAlign:"center"}}>*** Reviews ***</h1>
+    <p className="text-muted" style={{textAlign:"right"}}>Effective Date: 1st Aprils 2022</p><br/>
+    
+    
+    <div id="myDiv"> " Good Services - We have used services many times from Online Driver Hire. <br /> They are really helpful and charges are also reasonable as compare to others in market <br/>I am regular customer of Online Driver Hire ". </div>
+    <h4 className="card-title" style={{textAlign:"center"}}>-miss Anita N  </h4> <br/><br/><br/>
 
-                                <h3 className="card-title">"SERVICES"</h3>
-                                 <p className="text-muted">	- Driver On Hire is currently offering its services at specific cities in India.</p>
-                                 <p className="text-muted">- We provide our clients a safe, dependable and comfortable alternative to driving when they would rather not.</p>
-                                 <p className="text-muted">- SafeDrive main aim is to supply professional well trained car drivers to private and official driver as per requirements of people.</p>
-                                 <p className="text-muted">- Well trained car drivers are available for national approach.</p>                               
-                                 <p className="text-muted">- We provide the services of well trained chauffeurs who have mastered the art of safely wading through the intense traffic.</p>
-                                 <p className="text-muted">- Our motto is to provide you with best quality trained and experienced car drivers at your door steps.</p>
+    <div id="myDiv"> - " Have hired drivers from Online Driver Hire since last 4-5 years at least.👍 <br /> Barring one occasion, when a driver got 10 mins late, I have always seen drivers reporting on time and behaving nicely  ". </div>
+    <h4 className="card-title" style={{textAlign:"center"}}>-	 Manoj S. & Ann_a  </h4><br/>
 
-                                <h3 className="card-title">"EASY TO BOOK"</h3>
-                                 <p className="text-muted">- Power to Choose your Price | It is the first app where customer has a power to share their Price offer with registered Driver.You also have a power to choose your services and trips.</p>
-                                 <p className="text-muted">- Most importantly, you can speak to your service provider before confirming your booking so that you can clear all your doubts.</p>
-                                 <p className="text-muted">- Driver On Hire doesnt charge any commission from partners thus ensuring best price for your journey.</p>
-                                 <p className="text-muted">- It is an online Marketplace where various service providers across India register Driver and get listed after strict verification process.</p>
-                                 <p className="text-muted"> Partner vehicle goes out of the system if any of the document is expired such as Insurance, Tourist Permit, Fitness and cannot accept any booking until partner submits valid documents.</p>
-                                 <p className="text-muted">- Customers can get instant quotation based on their itinerary ( multi city travel , Oneway or Local ) with complete price breakup with zero advance payment.</p>
-                                 <p className="text-muted">- Booking process is simple : Enter Journey Details, Choose your Price and Done.</p>
-                                 <p className="text-muted">- Customers can book their Driver using website www.driveronhire.com .</p>
+    <div id="myDiv"> - Very very good & polite ❤️ !!*** </div>
+    <h4 className="card-title" style={{textAlign:"center"}}>-	 Dinesh Sharma  </h4><br/><br/>
 
-                                <h3 className="card-title">"OUR TEAM OUR FAMILY"</h3>
-                                 <p className="text-muted">- We strongly committed towards better life of our team with felixible timing, on time payments and continuesly increment on their earnings along with other benifits such as insurance and provident funds. We believe in.</p>
-                                 <p className="text-muted">- Happy Team, Quality Services</p>
-                                 <p className="text-muted">- Personal and Professional Training</p>
+    <div id="myDiv">- " Ashish tiwari was a good driver . Good driving skill and well behaved " .</div>
+    <h4 className="card-title" style={{textAlign:"center"}}>- Amit Patil </h4><br/><br/>
 
-                                <h3 className="card-title">"RELIABILITY"</h3>
-                                 <p className="text-muted">- The most affordable services of driver are available at your beck and call with complete transparency in pricing and prompt delivery of service. If you need driver for Chauffeur , city driving, office drops and pick-up for regular service, outstation trips, driver service at night to pick up clients from a late night party or late evening and drop them at their home safely. The driver service can also be availed for valet parking in commercial So next time a driver is required without hesitation you just make the booking and avail trained services of driver at your place.You are relaxed and just gets into the car of your choice and enjoy the ride back.</p>
-
-                                <h3 className="card-title">OUR CITIES</h3>
-                                <p className="text-muted"> Currently, we are operating our Daily Driver Service, Airport Driver Service, Outstation Driver Service, and Hourly Driver Rental Service All major Cities . We firmly believe in rendering safe and comfortable rides to our customers.</p>
-                                 
-
-                                <h3 className="card-title">OUR MISSIONS</h3>
-                                 <p className="text-muted">- Our first and foremost aim is to win the hearts of our prospects, value their precious time and money, and become India's one of the most trusted and loved cab service. This may take time, but our team is relentlessly working to make each ride impeccable.
-                                        We want to encourage a system of transparency in pricing and hence dealing with an idea to make it a reality.
-                                        We want to be the leader in delivering ergonomic and functional service to everyone.
-                                        Aiming towards offering an immersive and enriched riding experience for an enjoyable and wholesome experience.
-                                        We are leaving no stone unturned in becoming a pioneer in the vehicle rental industry by completely focusing on customers, partner growth, innovation, and efficiency.</p>
-
-                                  <h3 className="card-title">Why Ride With Us?</h3>
-                                 <p className="text-muted">- Riding with us will offer you a plethora of benefits and gives you the assurance of great experience.How?Have a look</p>
-
-                                  <h3 className="card-title">Convenient Pick-ups and Drop-offs</h3>
-                                 <p className="text-muted">- No matter where you are, book our cab and it will be available in a quick span of time. Head towards your office, tour, airport, or station in a convenient manner.</p>
-
-                                  <h3 className="card-title">Round The Clock Support and Assistance</h3>
-                                 <p className="text-muted">- In case you encounter any emergency situation like a vehicle breakdown, tyre puncture, or any other safety concern during your ride, we're just a call away at any hour of the day.</p>
-
-                                  <h3 className="card-title">Partner Verification</h3>
-                                  <p className="text-muted">- You can rest be assured that partners are 100% reliable, professional, and trained to cater the needs of every type of customer.</p>
-
-                                  <h3 className="card-title">Customer Satisfaction</h3>
-                                 <p className="text-muted">- Our service aims to ensure optimum customer satisfaction by offering safe, comfortable, and reliable rides.</p>
-
-                                 <h3 className="card-title">Know Everything Before you get in the Car. Checkout the Ratings and Reviews from the Past Rides.</h3>
-                                 <p className="text-muted">- Driving? Rather book a cab and enjoy the journey.
-                                                            Riding? Ride with us at affordable rates. Once you use it, you'll never go back.
-                                                            You don't need to pay until the ride is complete!
-                                                            Last minute? Perfect! We offer rapid booking.</p>
-
- 
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-        )
-    }
+    <div id="myDiv">- " I had availed the services of a driver from Drivers in India , <br /> Today i had the opportunity to get the service of Rohit Landge,<br /> Who i found extremely professional and and experienced driver, <br /> besides being well mannered and polite .I would strongly recommend him for drivers service."  </div>
+    <h4 className="card-title" style={{textAlign:"center"}}>- Riya Pal </h4><br /> <br /> 
+    
+    <div id="myDiv">- " I had my own personalised tour planned in which <br /> I wanted a driver to take me and my wife drop at Airport Hotel.
+                        This was arranged by Mr. Rafiq.<br /> 
+                        I checked with a few other local transport agencies and they all had higher rates....<br /> 
+                        I even constantly questioned Mr. Rafiq alot ...👍 but he always answered my questions nicely and humbly.<br /> 
+                        He never got frustrated at my constant queries and gave me all the detail that I asked for.<br /> 
+                        They picked us up right on time and dropped us at our hotel... it was a good drive.<br /> 
+                        Definitely go for this company.... trust me, I did a lot of research and these guys are the best in service and in rates...<br />
+                        Thanks Driver on Hire ❤️  </div>
+    <h4 className="card-title" style={{textAlign:"center"}}>- Talik Khan </h4><br /> <br /> 
+    
+    <div id="myDiv"> - " We travelled from Mumbai to Hyderabad. <br /> 
+                        Driver, Deepak Lad was excellent.<br /> 
+                        He was extremely courteous and drove very safely.
+                        In future, whenever we require the services of a driver, we will ask for him.👍 "
+    <h4 className="card-title" style={{textAlign:"center"}}>- Angelus Francis Xavie </h4>
+                      
+                       </div>
+                      </div>
+  );
 }
 
-export default TnC;
+export default App;
