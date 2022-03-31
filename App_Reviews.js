02b9161ea7375a8d4602643321c6382798eaddb8
paragtaide <89142951+paragtaide@users.noreply.github.com>
2022-03-31 18:57:05 +0530

    Add files via upload

---
+++ App_Reviews.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="card-body"><br/>
+    <h1 className="card-title" style={{textAlign:"center"}}>*** Reviews ***</h1>
+    <p className="text-muted" style={{textAlign:"right"}}>Effective Date: 1st Aprils 2022</p><br/>
+    
+    
+    <div id="myDiv"> " Good Services - We have used services many times from Online Driver Hire. <br /> They are really helpful and charges are also reasonable as compare to others in market <br/>I am regular customer of Online Driver Hire ". </div>
+    <h4 className="card-title" style={{textAlign:"center"}}>-miss Anita N  </h4> <br/><br/><br/>
+
+    <div id="myDiv"> - " Have hired drivers from Online Driver Hire since last 4-5 years at least.👍 <br /> Barring one occasion, when a driver got 10 mins late, I have always seen drivers reporting on time and behaving nicely  ". </div>
+    <h4 className="card-title" style={{textAlign:"center"}}>-	 Manoj S. & Ann_a  </h4><br/>
+
+    <div id="myDiv"> - Very very good & polite ❤️ !!*** </div>
+    <h4 className="card-title" style={{textAlign:"center"}}>-	 Dinesh Sharma  </h4><br/><br/>
+
+    <div id="myDiv">- " Ashish tiwari was a good driver . Good driving skill and well behaved " .</div>
+    <h4 className="card-title" style={{textAlign:"center"}}>- Amit Patil </h4><br/><br/>
+
+    <div id="myDiv">- " I had availed the services of a driver from Drivers in India , <br /> Today i had the opportunity to get the service of Rohit Landge,<br /> Who i found extremely professional and and experienced driver, <br /> besides being well mannered and polite .I would strongly recommend him for drivers service."  </div>
+    <h4 className="card-title" style={{textAlign:"center"}}>- Riya Pal </h4><br /> <br /> 
+    
+    <div id="myDiv">- " I had my own personalised tour planned in which <br /> I wanted a driver to take me and my wife drop at Airport Hotel.
+                        This was arranged by Mr. Rafiq.<br /> 
+                        I checked with a few other local transport agencies and they all had higher rates....<br /> 
+                        I even constantly questioned Mr. Rafiq alot ...👍 but he always answered my questions nicely and humbly.<br /> 
+                        He never got frustrated at my constant queries and gave me all the detail that I asked for.<br /> 
+                        They picked us up right on time and dropped us at our hotel... it was a good drive.<br /> 
+                        Definitely go for this company.... trust me, I did a lot of research and these guys are the best in service and in rates...<br />
+                        Thanks Driver on Hire ❤️  </div>
+    <h4 className="card-title" style={{textAlign:"center"}}>- Talik Khan </h4><br /> <br /> 
+    
+    <div id="myDiv"> - " We travelled from Mumbai to Hyderabad. <br /> 
+                        Driver, Deepak Lad was excellent.<br /> 
+                        He was extremely courteous and drove very safely.
+                        In future, whenever we require the services of a driver, we will ask for him.👍 "
+    <h4 className="card-title" style={{textAlign:"center"}}>- Angelus Francis Xavie </h4>
+                      
+                       </div>
+                      </div>
+  );
+}
+
+export default App;
